57f4ccc4c058277808f52536eb2e91729e65346a
Emilio <emiliojoaquinortizmalich@gmail.com>
2025-04-07 10:53:27 -0300

    instalacion de express, cors y la peticion get

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())//profe en la documentacion no pide cors pero me estaba dando error y lo tive que agregar
+
+app.get("/saludo/:nombre",(req,res)=>{
+    const nombre=req.params.nombre;
+    res.json({mensaje:`Hola ${nombre}`})
+
+})  
+
+app.listen(PORT,()=>{
+    console.log(`servidor andando en el puerto ${PORT}`)
+})
